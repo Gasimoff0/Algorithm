c6a201428de0d335b0bfebcc2b3a7b04e9c7ff34
Gasimoff0 <mddska.01@icloud.com>
2025-04-24 02:19:34 +0400

    added task

---
+++ MyProject/MyProject/Program.cs
@@ -0,0 +1,41 @@
+﻿#region Bolunme
+//verilmish ededin 3-e bolunub bolunmediyini tapan alqoritm
+
+//int a = 9;
+//if (a%3 == 0)
+//{
+//    Console.WriteLine("bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("bolunmur");
+//}
+#endregion
+
+#region araliq
+//verilmish ededin 10-dan boyuk ve 15-den kicik olub olmadigini tapan alqoritm
+
+//int a = 19;
+//if (a>10 && a<15)
+//{
+//    Console.WriteLine("10 ile 15 arasindadir");
+//}
+//else
+//{
+//    Console.WriteLine("10 ile 15 arasinda deyil");
+//}
+#endregion
+
+#region ikili bolunme
+//Verilmish n ededinin 3-e ve 7-e bolunub bolunmediyini tapan alqoritm qurun
+
+//int n = 29;
+//if (n%3 == 0 && n%7 ==0 )
+//{
+//    Console.WriteLine("bolunur");  
+//}
+//else
+//{
+//    Console.WriteLine("bolunmur");
+//}
+#endregion
